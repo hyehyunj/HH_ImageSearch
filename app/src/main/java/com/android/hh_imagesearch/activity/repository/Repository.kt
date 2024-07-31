a0ded20e680f3cc7249d40eba7774648704759a6
hyehyunj <developh620@gmail.com>
2024-07-31 09:36:24 +0900

    feature: 디벨롭브랜치 생성, view binding, viewmodel sync

---
+++ app/src/main/java/com/android/hh_imagesearch/activity/repository/Repository.kt
@@ -0,0 +1,12 @@
+package com.android.hh_imagesearch.activity.repository
+
+class Repository {
+
+    private val _repository = mutableListOf<>()
+    val repository : List
+        get() = _repository.toList()
+
+    fun addRepository() {
+        if
+    }
+}
---
+++ app/src/main/java/com/android/hh_imagesearch/activity/viewmodel/MainViewModel.kt
@@ -0,0 +1,18 @@
+package com.android.hh_imagesearch.activity.viewmodel
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class MainViewModel(private val repository: ) : ViewModel() {
+    private val _LiveData : MutableLiveData<Int> = MutableLiveData()
+    val liveData : LiveData<Int> = _LiveData
+
+    init {
+        loadData()
+    }
+
+    private fun loadData() {
+        _LiveData.value = repository.fa
+    }
+}
